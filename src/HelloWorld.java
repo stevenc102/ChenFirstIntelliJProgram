9e01fd09480078e840902775249d2bffc22addef
1E10 laptop <sample@bths.edu>
2022-09-22 11:14:12 -0400

    added new println

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello world!!!!!!!!!!!!");
+        System.out.println("Hello world!!!!!!!!!!!!");
+        System.out.println("Goodbye World!!!!");
     }
 }
